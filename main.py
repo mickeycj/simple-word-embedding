18766f73b2ecd249d5335b2657bc7d03bd21d003
Chanon Jenakom <chanonjenakom@gmail.com>
2019-01-28 17:56:55 +0700

    Use gensim model

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import sys
 from gensim.utils import simple_preprocess
 import pandas as pd
-from word2vec import Word2Vec
 
 def tokenize(document):
     return simple_preprocess(str(document).encode("utf-8"))
@@ -10,27 +9,25 @@ if __name__ == "__main__":
     settings = {}
     settings["n"] = int(sys.argv[1])
     settings["window_size"] = int(sys.argv[2])
-    settings["learning_rate"] = float(sys.argv[3])
-    settings["epochs"] = int(sys.argv[4])
 
     print("Creating corpus...")
     corpus = []
-    documents = pd.read_csv("./data/train.csv")[["question1", "question2", "is_duplicate"]].sample(n=50000).reset_index(drop=True)
+    documents = pd.read_csv("./data/train.csv")[["question1", "question2", "is_duplicate"]].sample(frac=1).reset_index(drop=True)
     for index, row in documents.iterrows():
-        print("Tokenize row {}".format(index))
         corpus.append(tokenize(row["question1"]))
         if row["is_duplicate"] == 0:
             corpus.append(tokenize(row["question2"]))
-
-    w2v = Word2Vec(settings)    
+    
+    from gensim.models import Word2Vec   
+    w2v = Word2Vec(size=settings["n"], window=settings["window_size"], min_count=2, sg=1, workers=10)
 
     print("Creating training data...")
-    training_data = w2v.generate_training_data(corpus)
+    w2v.build_vocab(corpus)
 
     print("Training...")
-    w2v.train(training_data)
+    w2v.train(sentences=corpus, total_examples=len(corpus), epochs=w2v.epochs)
 
-    for word in ['the','quick','brown','fox','jumped','over','lazy','dog']:
+    for word in ['the', 'quick', 'brown', 'fox', 'jumped', 'over', 'lazy', 'dog']:
         print(word)
-        for word, sim in w2v.word_sim(word, 3):
+        for word, sim in w2v.wv.most_similar(positive=word, topn=5):
             print(word, sim)
